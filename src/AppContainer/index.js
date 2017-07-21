6427a02295cdbaab55608a4be7042b33892a06a8
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-21 19:50:09 +0200

    file structure and redux setup

--- src/AppContainer/index.js
+++ src/AppContainer/index.js
@@ -1,6 +1,8 @@
 import React, { Component, PopTypes } from "react";
 import { Router } from "react-native-router-flux";
 
+import scenes from "../routes/scenes";
+
 import { Provider } from "react-redux";
 
 export default class AppContainer extends Component {
