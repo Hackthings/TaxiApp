14adcf2884665ad0f8f7b897954cb3a644f89080
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-22 22:00:51 +0200

    add second input field:

--- src/routes/Home/components/SearchBox/index.js
+++ src/routes/Home/components/SearchBox/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text } from "react-native";
 import { View, InputGroup, Input } from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import styles from "./SearchBoxStyles.js";
 
@@ -8,12 +9,26 @@ export const SearchBox = ()=>{
 
 	return(
 		<View style={styles.searchBox}>
-			<Text style={styles.label}>
-				Pick Up
-			</Text>
-			<InputGroup>
-				<Input style={styles.inputSearch} placeholder="Choose pick-up location"/>
-			</InputGroup>
+			
+			<View style={styles.inputWrapper}>
+				<Text style={styles.label}>
+					Pick Up
+				</Text>
+				<InputGroup>
+					<Icon name="search" size={15} color="#FF5E3A"/>
+					<Input style={styles.inputSearch} placeholder="Choose pick-up location"/>
+				</InputGroup>
+			</View>
+
+			<View style={styles.secondInputWrapper}>
+				<Text style={styles.label}>
+					Drop Off
+				</Text>
+				<InputGroup>
+					<Icon name="search" size={15} color="#FF5E3A"/>
+					<Input style={styles.inputSearch} placeholder="Choose drop-off location"/>
+				</InputGroup>
+			</View>
 		</View>
 	);
 		
