27f24b798f94d94fd6340bf3c93afa140471cac2
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-22 23:02:55 +0200

    create component search results

---
+++ src/routes/Home/components/SearchResults/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text } from "react-native";
+import { View, InputGroup, Input } from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import styles from "./SearchResults.js";
+
+export const SearchResults = ()=>{
+
+	return(
+		<View>
+			
+		</View>
+	);
+		
+};
+
+export default SearchResults;
