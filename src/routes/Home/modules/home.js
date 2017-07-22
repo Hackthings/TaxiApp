ef8f1363eaeb3a8b7480155ca7ba3a0652bee004
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-22 20:42:45 +0200

    get current location

--- src/routes/Home/modules/home.js
+++ src/routes/Home/modules/home.js
@@ -1,25 +1,42 @@
 import update from "react-addons-update";
 import constants from "./actionConstants";
 
-const { SET_NAME } = constants;
 
-export function setName(){
-	return {
-		type:SET_NAME,
-		payload:"Tijmen"
+//-----------------------------
+// Constants
+//-----------------------------
+const { GET_CURRENT_LOCATION } = constants;
+
+//-----------------------------
+// Actions
+//-----------------------------
+export function getCurrentLocation(){
+	return(dispatch)=>{
+		navigator.geolocation.getCurrentPosition(
+			(position)=>{
+				dispatch({
+					type:GET_CURRENT_LOCATION,
+					payload: position
+				});
+			},
+			(error)=> console.log(error.message),
+			{enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+		);
 	}
 }
 
-function handleSetName(state, action){
+//-----------------------------
+// Action Handlers
+//-----------------------------
+function handleGetCurrentLocation(state, action){
 	return update(state, {
-		name:{
+		region:{
 			$set:action.payload
 		}
-	});
+	})
 }
-
 const ACTION_HANDLERS = {
-	SET_NAME:handleSetName
+	GET_CURRENT_LOCATION:handleGetCurrentLocation
 }
 const initialState = {};
 
